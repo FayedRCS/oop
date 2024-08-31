65401d12f2de97656e0ca2ee83bba6a4a9339efa
fayed <fayed_rana@outlook.com>
2024-08-31 23:21:20 +0200

    All challenges complete

--- ch2challenges/Pupil_records_test.py
+++ ch2challenges/Pupil_records_test.py
@@ -1,12 +1,38 @@
 class Student:
     def __init__(self, name):
-        pass
+        self.name = name
+        self.__courses = {}
 
     def calculate_letter_grade(self, score):
-        pass
+        self.score = score
+
+        if score >= 90:
+            return "A"
+        elif score >= 80 and score <= 89:
+            return "B"
+        elif score >= 70 and score <= 79:
+            return "C"
+        elif score >= 60 and score <= 69:
+            return "D"
+        else: 
+            return "F"
 
     def add_course(self, course_name, score):
-        pass
+        self.course_name = course_name
+        self.score = score
+
+        score = self.calculate_letter_grade(score)
+        
+        self.__courses[course_name] = score
+            
 
     def get_courses(self):
-        pass
+        return self.__courses
+
+def main():
+    student = Student("jenn")
+
+    print(student.add_course("math", 79))
+
+main()
+
