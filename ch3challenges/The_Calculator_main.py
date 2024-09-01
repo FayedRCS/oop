7518185ccaf3d2cc32605aadbaf818f984ff169d
fayed <fayed_rana@outlook.com>
2024-09-01 21:23:03 +0200

    Challenge contents

---
+++ ch3challenges/The_Calculator_main.py
@@ -0,0 +1,30 @@
+class Calculator:
+    def __init__(self):
+        pass
+
+    def add(self, a):
+        pass
+
+    def subtract(self, a):
+        pass
+
+    def multiply(self, a):
+        pass
+
+    def divide(self, a):
+        pass
+
+    def modulo(self, a):
+        pass
+
+    def power(self, a):
+        pass
+
+    def square_root(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def get_result(self):
+        pass
