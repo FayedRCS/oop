49ea80ece49d6e76582ce2077b921c5e7770b680
fayed <fayed_rana@outlook.com>
2024-09-03 14:38:41 +0200

    lessons

---
+++ chapter_4/multiple_children/main.py
@@ -0,0 +1,22 @@
+class Hero:
+    def __init__(self, name, health):
+        self.__name = name
+        self.__health = health
+
+    def get_name(self):
+        return self.__name
+
+    def get_health(self):
+        return self.__health
+
+    def take_damage(self, damage):
+        self.__health -= damage
+
+
+class Archer(Hero):
+    def __init__(self, name, health, num_arrows):
+        super().__init__(name, health)
+        
+
+    def shoot(self, target):
+        pass
